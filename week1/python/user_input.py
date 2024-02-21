2bd9d14923e9c8387ff32a27a1a6bd82da140e9a
brayohmurithidev <murithibrianm@gmail.com>
2024-02-21 23:49:35 +0300

    created user_input.py and added code

---
+++ week1/python/user_input.py
@@ -0,0 +1,5 @@
+name = input('Please Input your name\n')
+age = input('Input your age\n')
+location = input('Input your location\n')
+
+print ('Hello ' + name + ' your age '+ age + ' years old and you live in ' + location + '.')
