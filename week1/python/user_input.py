312bace324fa4b86213963602db6fe406ca53c8b
brayohmurithidev <murithibrianm@gmail.com>
2024-02-23 21:13:06 +0300

    added line 2

--- week1/python/user_input.py
+++ week1/python/user_input.py
@@ -3,3 +3,4 @@ age = input('Input your age\n')
 location = input('Input your location\n')
 
 print ('Hello ' + name + ' your age '+ age + ' years old and you live in ' + location + '.')
+print('line 2')
